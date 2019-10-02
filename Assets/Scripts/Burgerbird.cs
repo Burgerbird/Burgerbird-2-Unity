2b5f38645e45bf7e42000d435284dcf15859f768
Sebastian Friedrich <dev@sebastianfriedrich.at>
2019-10-03 01:04:38 +0200

    added regions

--- Assets/Scripts/Burgerbird.cs
+++ Assets/Scripts/Burgerbird.cs
@@ -4,53 +4,47 @@ using UnityEngine;
 
 public class Burgerbird : MonoBehaviour
 {
-    [SerializeField] private float minSpeed = 3f;
-    [SerializeField] private float maxSpeed;
+    #region Declaration
     [SerializeField] private float gravityInput = 3f;
+    [SerializeField] private float minVelocityX = 3f;
+    [SerializeField] private float maxVelocityY = 3f;
+
+    [Tooltip("Square Magnitude of Velocity (Square Speed)")]
+    [SerializeField] float camZoom = 300f;
     private float gravityPlus;
     private float defaultGravity;
-    Vector2 burgerPosition = Vector2.zero;
-    Vector2 lastPosition = Vector2.zero;
-    Vector2 currentVelocity;
-    Vector2 oldVelocity;
-    private float sqrSpeed; // "real" speed (square vector length per fixed update duration)
+    private Vector2 velocity;
     private Rigidbody2D burgerbird;
-
+    #endregion
     void Start()
     {
         burgerbird = GetComponent<Rigidbody2D>();
         defaultGravity = burgerbird.gravityScale;
-        Vector2 burgerPosition = new Vector2(transform.position.x, transform.position.y);
-        lastPosition = burgerPosition;
-        oldVelocity = currentVelocity = burgerbird.velocity;
     }
 
     void Update()
     {
+        #region Add Gravity
         float verticalInput = Input.GetAxisRaw("Vertical");
         if (verticalInput <= 0)
-        {
             gravityPlus = Mathf.Abs(verticalInput) * gravityInput;
-        }
         else
-        {
             gravityPlus = 0;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        // Add Gravity
         burgerbird.gravityScale = defaultGravity + gravityPlus;
-        // Minimum Speed
-        Vector2 speed = burgerbird.velocity;
-        if (speed.x <= minSpeed)
-        {
-            speed.x = minSpeed;
-            burgerbird.velocity = speed;
-        }
-        // square speed (faster than normal speed)
-        sqrSpeed = burgerbird.velocity.sqrMagnitude;
-        Debug.Log(sqrSpeed);
+        #endregion
+
+        #region Minimum Velocity
+        velocity = burgerbird.velocity;
+        if (velocity.x <= minVelocityX)
+            burgerbird.velocity = new Vector2(minVelocityX, velocity.y);
+        #endregion
+
+        #region Cam Zoom
+        // Switch...
+        if (velocity.sqrMagnitude > camZoom)
+            Debug.Log("Velocity Square Magnitude higher than: " + camZoom);
+        else
+            Debug.Log("Zoom in!");
+        #endregion
     }
 }
